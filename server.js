ac1388304a4063c99ff3191ba84f1dcbed49beec
ghrandi <93522718+ghrandi@users.noreply.github.com>
2023-11-06 17:28:55 +0100

    Update server.js

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.get("/", (req, res) => {
 
 require("./app/routes/mat.routes.js")(app);
 require("./app/routes/smat.routes.js")(app);
-
+require("./app/routes/Thou.routes.js")(app);
 
 
 // Get the port number from the environment variable or use port 3000 as a default
