f1157409e2a0efbcb05f2ead9c67d312a4fd635d
ghrandi <93522718+ghrandi@users.noreply.github.com>
2023-11-12 21:34:11 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require("express");
-var cors = require("cors");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 
 // create express app
 const app = express();
-app.use(cors());
+
+// Set up CORS for a specific origin
+const allowedOrigin = "https://fsegs.netlify.app";
+const corsOptions = {
+  origin: allowedOrigin,
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -13,7 +23,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Configuring the database
-
 const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
@@ -23,18 +32,17 @@ mongoose.connect("mongodb+srv://malekghrandii:gryRkDvagNbuWXS6@cluster0.zstqfyj.
   useNewUrlParser: true, // Use the new URL parser
   useUnifiedTopology: true, // Use the new Server Discover and Monitoring engine
 })
-.then(() => {
-  console.log("Successfully connected to the database");
-})
-.catch((err) => {
-  console.error("Error connecting to the database: ", err);
-});
+  .then(() => {
+    console.log("Successfully connected to the database");
+  })
+  .catch((err) => {
+    console.error("Error connecting to the database: ", err);
+  });
 
 // define a simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to Rest API By Malek Ghrandi." });
 });
-
 
 require("./app/routes/mat.routes.js")(app);
 require("./app/routes/smat.routes.js")(app);
@@ -42,6 +50,8 @@ require("./app/routes/thou.routes.js")(app);
 require("./app/routes/content.routes.js")(app);
 
 // Get the port number from the environment variable or use port 3000 as a default
-app.listen(process.env.PORT||3000, () => {
-  console.log(`Server is listening on port 3000`);
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
 });
